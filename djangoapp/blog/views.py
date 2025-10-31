682cc64eafd807f0c44802f43da3988998c2b9ff
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-31 09:07:34 -0300

    Usando context_processors para injetar valores em todos os templates do Django

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        context={
+            'nome': 'Ygor Lima'
+        }
     )
