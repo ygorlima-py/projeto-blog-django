3e1cb7898fa404e021bd83fb95b289cead6f9177
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-03 06:52:23 -0300

    Escrevendo o html de base, page, posts e os trechos em partials e o css utilizado

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,29 +1,45 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 # Create your views here.
 def index(request):
-    return render(
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(  
         request,
         'blog/pages/index.html',
-        context={
-            'nome': 'Ygor Lima'
+        context= {
+            'page_obj': page_obj,
         }
     )
 
 def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/pages/post.html',
+
         context={
-            'nome': 'Ygor Lima'
+            'page_obj': page_obj,
         }
     )
 
 def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/pages/page.html',
         context={
-            'nome': 'Ygor Lima'
+            'page_obj': page_obj,
         }
     )
