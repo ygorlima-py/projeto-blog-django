6b5679a0eef76412ecc39d07e1b11f496806981d
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-03 10:06:25 -0300

    Criando o model Page, e o admin PageAdmin

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Category
+from blog.models import Tag, Category, Page
 
 
 @admin.register(Tag)
@@ -18,8 +18,20 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
     list_display_links = 'name',
     search_fields = 'id', 'name', 'slug',
-    list_per_page = 10
+    list_per_page = 50
     ordering = '-id',
     prepopulated_fields = {
         "slug": ('name', ),
+    }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'slug', 'is_published', 'content'
+    list_display_links = 'title',
+    search_fields = 'id', 'title', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title', ),
     }
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -19,6 +19,9 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 3)
         return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
 
 
 class Category(models.Model):
@@ -39,3 +42,34 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 3)
         return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Page(models.Model):
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
+    title = models.CharField(max_length=50,)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+
+    is_published = models.BooleanField(
+        default = False,
+        help_text='Este campo precisará estar marcado para a pagina ser exibida públicamente'
+        )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 3)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
