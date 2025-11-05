95b0b07589a62b367b3802b370812d88973949bd
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-05 09:13:03 -0300

    Trabalhando com Class Based, transformando a view index em classe

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from blog.views import index, page, post, created_by, category, tag, search
+from blog.views import PostListView, page, post, created_by, category, tag, search
+
+# Para mais detalhes sobre urls de classes:
+# Consultar a documentação em: https://docs.djangoproject.com/pt-br/4.2/ref/class-based-views/generic-display/#listview
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('page/<slug:slug>/', page, name='page'), # type: ignore
     path('post/<slug:slug>/', post, name='post'), 
     path('created_by/<int:author_pk>/', created_by, name='created_by'), 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,28 +4,65 @@ from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404
+from django.views.generic.list import ListView
+'''
+Views de exibição genéricas
 
-POSTS_PER_PAGE = 9 
+É uma lista? ou em um único objeto só?
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.get_published() # type: ignore
+Se for lista  use ListView
+Se for um único objeto só use DetailView
+
+Para mais detalhes consulte: https://docs.djangoproject.com/pt-br/4.2/ref/class-based-views
+'''
+
+POSTS_PER_PAGE = 1 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/pages/index.html' 
+    context_object_name = 'posts'
+    ordering = '-pk',
+    paginate_by = POSTS_PER_PAGE
+    queryset = Post.objects.get_published() #type: ignore
+
+    # def get_queryset(self):
+
+    #     queryset = super().get_queryset()
+    #     print('antes: ', queryset)
+
+    #     queryset = queryset.filter(is_published=True)
+    #     print('depois: ', queryset)
+
+    #     return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            'page_title': 'home - ' 
+        })
+        return context
 
 
-    print(posts)
+# def index(request):
+#     posts = Post.objects.get_published() # type: ignore
 
-    paginator = Paginator(posts, POSTS_PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
-    return render(  
-        request,
-        'blog/pages/index.html',
-        context= {
-            'page_obj': page_obj,
-            'page_title': 'Home - ',
-        }
-    )
+#     print(posts)
+
+#     paginator = Paginator(posts, POSTS_PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(  
+#         request,
+#         'blog/pages/index.html',
+#         context= {
+#             'page_obj': page_obj,
+#             'page_title': 'Home - ',
+#         }
+#     )
 
 def post(request, slug):
 
