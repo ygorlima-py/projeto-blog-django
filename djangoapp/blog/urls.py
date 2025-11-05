0816c02ee6f84bfc6fdeb827dcc4894aa1de92b1
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-05 05:02:50 -0300

    Configurando model Page e injetando valores em Pages

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('page/', page, name='page'),
+    path('page/<slug:slug>/', page, name='page'),
     path('post/<slug:slug>/', post, name='post'), 
     path('created_by/<int:author_pk>/', created_by, name='created_by'), 
     path('category/<slug:slug>/', category, name='category'), 
