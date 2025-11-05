d84db1db9656f829f26638b4d93f977d584bbc29
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-05 13:31:05 -0300

    Estruturando a view created_by em class Based e entendendo o comportamento dos métodos herdados de PostListView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from blog.views import PostListView, page, post, created_by, category, tag, search
+from blog.views import (
+                        PostListView, 
+                        page, 
+                        post, 
+                        CreatedByListView, 
+                        category, 
+                        tag, 
+                        search
+                        )
+
 
 # Para mais detalhes sobre urls de classes:
 # Consultar a documentação em: https://docs.djangoproject.com/pt-br/4.2/ref/class-based-views/generic-display/#listview
@@ -10,7 +19,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('page/<slug:slug>/', page, name='page'), # type: ignore
     path('post/<slug:slug>/', post, name='post'), 
-    path('created_by/<int:author_pk>/', created_by, name='created_by'), 
+    path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'), 
     path('category/<slug:slug>/', category, name='category'), # type: ignore
     path('tag/<slug:slug>/', tag, name='tag'), # type: ignore
     path('serarch/', search, name='search'), 
