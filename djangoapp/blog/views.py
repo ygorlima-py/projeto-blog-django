ede7554dd10e58a33b32eb3fce9ec99b4c1d2328
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-05 14:47:12 -0300

    Migando a view category para Listview e usando allow_empty para tratar erros 404

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.http import Http404, HttpRequest, HttpResponse
 from django.views.generic.list import ListView
 from typing import Any
+
 '''
 Views de exibição genéricas
 
@@ -51,36 +52,6 @@ def post(request, slug):
         }
     )
 
-def created_by(request, author_pk):
-    user = User.objects.filter(pk=author_pk).first()
-
-    if user is None:
-        raise Http404()
-    
-    posts = Post.objects.get_published().filter(created_by__pk=author_pk) # type: ignore
-
-    user_full_name = user.username 
-
-    if user.first_name:
-        user_full_name = f'{user.first_name} {user.last_name}'
-
-    page_title = f'Posts de {user_full_name} - '
-
-    print(posts)
-
-    paginator = Paginator(posts, POSTS_PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(  
-        request,
-        'blog/pages/index.html',
-        context= {
-            'page_obj': page_obj,
-            'page_title': page_title,
-
-        }
-    )
 
 class CreatedByListView(PostListView):
     def __init__(self, **kwargs: Any) -> None:
@@ -91,7 +62,7 @@ class CreatedByListView(PostListView):
         context = super().get_context_data(**kwargs)
         user = self._temp_context['user']
         user_full_name = user.username
-     
+    
         if user.first_name:
             user_full_name = f'{user.first_name} {user.last_name}'
 
@@ -100,7 +71,6 @@ class CreatedByListView(PostListView):
         context.update({'page_title': page_title,})
 
         return context
-
 
     def get_queryset(self, **kwargs) -> dict[str, Any]:
         qs = super().get_queryset()
@@ -122,32 +92,24 @@ class CreatedByListView(PostListView):
         return super().get(request, *args, **kwargs)
         
 
+class CategoryListView(PostListView):
+    allow_empty = False
 
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        qs = super().get_queryset()
+        qs = qs.filter(category__slug=slug)
 
-
-def category(request, slug):
-    posts = Post.objects.get_published().filter(category__slug=slug) # type: ignore
+        return qs
     
-    print(posts)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_name = self.object_list[0].category.name # type: ignore 
+        page_title = f'{category_name} - Categoria - '
+        context.update({'page_title': page_title})
 
-    paginator = Paginator(posts, POSTS_PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+        return context
 
-    if len(page_obj) == 0:
-        raise Http404
-    
-    page_title = f'{page_obj[0].category.name} - Categoria - '
-
-    return render(  
-        request,
-        'blog/pages/index.html',
-        context= {
-            'page_obj': page_obj,
-            'page_title': page_title,
-
-        }
-    )
 
 def tag(request, slug):
     posts = Post.objects.get_published().filter(tags__slug=slug) # type: ignore
