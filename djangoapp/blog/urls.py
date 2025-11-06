9df57a155370b47cadce58434740b5bffa20ad61
Ygor Lima <ygor.limarsx@gmail.com>
2025-11-06 06:03:52 -0300

    Fazendo as ultimas class Based views de Post e Page, usando DetailView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from blog.views import (
                         PostListView, 
-                        page, 
-                        post, 
+                        PageDetailView, 
+                        PostDetailView, 
                         CreatedByListView, 
                         CategoryListView, 
                         TagListView, 
-                        search
+                        SearchListView,
                         )
 
 
@@ -17,11 +17,11 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
-    path('page/<slug:slug>/', page, name='page'), # type: ignore
-    path('post/<slug:slug>/', post, name='post'), 
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'), # type: ignore
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post'), 
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'), 
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'), # type: ignore
     path('tag/<slug:slug>/', TagListView.as_view(), name='tag'), # type: ignore
-    path('serarch/', search, name='search'), 
+    path('serarch/', SearchListView.as_view(), name='search'), 
 
 ]
