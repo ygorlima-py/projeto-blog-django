709c8b9c4474e14de3ce64ca51b6ab0099d7aede
Ygor Lima <ygor.limarsx@gmail.com>
2026-08-22 17:03:59 -0300

    Traduz rotas de categorias e páginas

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from blog.views import (
                         PostListView, 
                         PageDetailView, 
@@ -17,10 +18,20 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
-    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'), # type: ignore
+    path('pagina/<slug:slug>/', PageDetailView.as_view(), name='page'), # type: ignore
+    path(
+        'page/<slug:slug>/',
+        RedirectView.as_view(pattern_name='blog:page', permanent=True),
+        name='page_legacy',
+    ),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post'), 
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'), 
-    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'), # type: ignore
+    path('categoria/<slug:slug>/', CategoryListView.as_view(), name='category'), # type: ignore
+    path(
+        'category/<slug:slug>/',
+        RedirectView.as_view(pattern_name='blog:category', permanent=True),
+        name='category_legacy',
+    ),
     path('tag/<slug:slug>/', TagListView.as_view(), name='tag'), # type: ignore
     path('serarch/', SearchListView.as_view(), name='search'), 
 
